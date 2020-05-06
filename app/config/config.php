3f15ecb3b5d5e7709097f8215234b6d14f1071d5
jacc89 <laflacalex9@gmail.com>
2020-05-06 12:24:47 -0500

    helpers

--- app/config/config.php
+++ app/config/config.php
@@ -13,5 +13,5 @@ define('RUTA_APP', dirname(dirname(__FILE__)));
 //Ruta url ejemplo  http://localhost/FROND-END-SILTO/
 define('RUTA_URL','http://localhost/FROND-END-SILTO/');  //aqui ruta 
 
-define('NOMBRESITIO', 'SILTO');
+define('NOMBRESITIO', 'CRUD');
 ?>
